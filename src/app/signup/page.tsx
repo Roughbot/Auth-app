80a0995617a4654b92f66b12227a3b167fbba45b
Sathyanarayanan <sathyanarayanan1106@gmail.com>
2024-01-11 12:34:53 +0530

    nodemail code

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -44,7 +44,7 @@ export default function SignUpPage() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>{loading ? "Processing" : "SignUp"}</h1>
+      <h1 className="text-bold text-xl">{loading ? "Processing" : "SignUp"}</h1>
       <hr />
       <label htmlFor="username">UserName</label>
       <input
